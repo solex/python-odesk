263aa22863b65d6a8966deb5ba4589bfc488a7b5
volodymyr Hotsyk <gotsyk@gmail.com>
2010-07-09 14:58:33 +0300

    Fix to use both user_id and team_id in the hr.get_user_role

--- odesk/__init__.py
+++ odesk/__init__.py
@@ -401,7 +401,7 @@ class HR2(Namespace):
         data = {}
         if user_id:
             data = {'user__reference': user_id}
-        elif team_id:
+        if team_id:
             data = {'team__reference': team_id}
         data['include_sub_teams'] = sub_teams
         url = 'userroles'
